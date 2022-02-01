5df602431dc77bc5f8924e83de233cb783c196ee
원동현 <ehdgus5500@gmail.com>
2022-02-01 14:10:27 +0900

    프로젝트 생성

---
+++ Server/ClientBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
